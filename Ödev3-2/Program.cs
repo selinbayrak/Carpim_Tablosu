3d64fc7c71549cea0e2d55908701bc31a3d9dc91
AYSE BAYRAK <AYSE BAYRAK@LAPTOP-J2JFAAN1>
2024-11-17 22:02:52 +0300

    Proje dosyası ekle.

---
+++ Ödev3-2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+for(int a=1; a<11; a++)
+{
+    for(int b=1; b<6; b++)
+    {
+        Console.Write($"{b}x{a}={a * b}\t\t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n\n");
+
+for (int a = 1; a < 11; a++)
+{
+    for (int b = 6; b < 11; b++)
+    {
+        Console.Write($"{b}x{a}={a * b}\t\t");
+    }
+
+    Console.WriteLine();
+}
